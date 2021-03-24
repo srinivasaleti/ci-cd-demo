e9500219b6c2a161593999e15e5715f969a7b4bf
Srinivasarao Aleti <srinivas.aleti03@gmail.com>
2021-03-24 21:11:30 +0530

    Add lint check

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const handler = async function(event, context) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
   return "Hello"
 }
 
